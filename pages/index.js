56383b369fe7b793928a9a9f6fdd5c0efe20cdb7
Brandon <brandonlimjingzhi@gmail.com>
2022-10-16 02:46:50 +0800

    Added client side rendering example.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
 import { Button } from "antd";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/test")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data.message[0].name);
+      });
+  }, []);
+
+  if (!data) return <p>No profile data</p>;
+
   return (
     <div>
+      <p>Name: {data.message[0].name}</p>
       <Button type="primary">HELLO WORLD</Button>
     </div>
   );
