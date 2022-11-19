3d2f2bb113312a84c67c580451260d9f7d173309
brandonut <heelabc@gmail.com>
2022-11-19 19:04:52 +0800

    fixes

--- components/TripCardForm.js
+++ components/TripCardForm.js
@@ -85,25 +85,24 @@ export default function TripCardForm(props) {
                       <h3 className="font-medium">Editing {props.tripData[0].stopName} at {props.tripData[0].eventName}</h3>
                     </div>
                     <div className="flex-grow px-5 py-1">
-                      <div className="py-4 input-group">
-                        <h4 className="pb-1">Stop Name</h4>
-                        <input type="text" placeholder="Stop Name" className="w-full p-2 border-2 rounded-md focus:outline-sgg-blue" required />
+                    <div className="py-4 input-group">
+                      <div className="flex flex-row pb-1">
+                          <h4 className="w-2/3">Stop Name</h4>
+                          <h4 className="w-1/3 pl-2">Type</h4>
+                        </div>
+                        <div className="flex flex-row gap-4 ">
+                          <input type="text" placeholder="Stop Name" className="w-2/3 p-2 border-2 rounded-md focus:outline-sgg-blue" required />
+                          <input type="text" placeholder="Eat/Do/Stay" className="flex w-1/3 p-2 border-2 rounded-md input-group focus:outline-sgg-blue" required />
+                        </div>
                       </div>
-                      {/* <div className="grid grid-flow-row grid-cols-2 py-1"> */}
                       <div className="py-4 input-group">
                         <div className="flex flex-row pb-1">
                           <h4 className="w-2/3">Event Name</h4>
                           <h4 className="w-1/3 pl-2">Time</h4>
                         </div>
-                        {/* <div className="flex flex-col items-center justify-between gap-0 md:flex-row md:gap-0"> */}
                         <div className="flex flex-row gap-4 ">
                           <input type="text" placeholder="Event Name" className="w-2/3 p-2 border-2 rounded-md focus:outline-sgg-blue" required />
-                          <div className="flex w-1/3 p-2 border-2 rounded-md input-group">
-                          <input type="text" placeholder="Time" className="w-full focus:outline-sgg-blue" required />
-                          <span>
-                            <svg fill="rgb(209, 213, 219)" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M24 12c0 6.627-5.373 12-12 12s-12-5.373-12-12c0-.808.083-1.596.234-2.359h2.076c-.188.759-.31 1.543-.31 2.359 0 5.514 4.486 10 10 10s10-4.486 10-10-4.486-10-10-10c-2.234 0-4.292.744-5.957 1.989l2.049 2.049-7.012 1.354 1.354-7.013 2.183 2.183c2.036-1.598 4.594-2.562 7.383-2.562 6.627 0 12 5.373 12 12zm-13-6v8h7v-2h-5v-6h-2z" /></svg>
-                          </span>
-                          </div>
+                          <input type="text" placeholder="Time" className="flex w-1/3 p-2 border-2 rounded-md input-group focus:outline-sgg-blue" required />
                         </div>
                       </div>
                       <div className="py-4 input-group">
