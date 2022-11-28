c610b5949109ba37c59ebd02ed1376b23dbedd37
Brandon <brandonlimjingzhi@gmail.com>
2022-11-29 00:31:39 +0800

    remove unused imports

--- pages/api/search/review.js
+++ pages/api/search/review.js
@@ -1,23 +1,17 @@
-import { Router } from "express";
-import { Double } from "mongodb";
-const http = require('http');
-const url = require('url');
-
 const { connectToDatabase } = require("../../../libs/mongodb");
 
 export default async function handler(req, res) {
-  console.log('Method: ', req.method)
+  console.log("Method: ", req.method);
   if (req.method === "GET") {
-    const {review} = req.query
-    
+    const { review } = req.query;
+
     let { db } = await connectToDatabase();
 
     let reviewData = await db.collection("EVENTS").findOne({ ID: "E1" });
-    
+
     res.status(200).json(reviewData);
   } else if (req.method === "POST") {
-    if (!req.body["review"])
-      res.status(200).json({ results: false, success: true });
+    if (!req.body["review"]) res.status(200).json({ results: false, success: true });
     else {
       try {
         let { db } = await connectToDatabase();
@@ -32,9 +26,7 @@ export default async function handler(req, res) {
         const review = req.body.review;
 
         const reviewData = { review_id: reviewId, review_desc: review, email: email };
-        let response = await db
-          .collection("EVENTS")
-          .update({ ID: req.body.id }, { $push: { reviews: reviewData } });
+        let response = await db.collection("EVENTS").update({ ID: req.body.id }, { $push: { reviews: reviewData } });
         // console.log(response)
 
         if (response.modifiedCount > 0) {
@@ -44,21 +36,14 @@ export default async function handler(req, res) {
         }
       } catch (error) {
         console.log(error);
-        res
-          .status(200)
-          .json({ message: new Error(error).message, success: false });
+        res.status(200).json({ message: new Error(error).message, success: false });
       }
     }
   } else if (req.method === "DELETE") {
-
     let { db } = await connectToDatabase();
-    const response = await db.collection('EVENTS')
-        .findOne({ ID: "E1"})
+    const response = await db.collection("EVENTS").findOne({ ID: "E1" });
     const { reviews } = response;
-    const newData = reviews.filter(r => r.review_id !== req.body.review_id);
-    const response2 = await db.collection('EVENTS')
-        .update({ ID: "E1" }, { $set: { reviews: newData } });
-
-
+    const newData = reviews.filter((r) => r.review_id !== req.body.review_id);
+    const response2 = await db.collection("EVENTS").update({ ID: "E1" }, { $set: { reviews: newData } });
   }
 }
