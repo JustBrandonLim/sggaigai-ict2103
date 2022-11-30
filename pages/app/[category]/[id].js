3714aacfaa567c1cd361bfd836d43ddf7cc3c12a
p1816246 <khorchai@hotmail.co.uk>
2022-11-30 20:52:44 +0800

    minor styling on review

--- pages/app/[category]/[id].js
+++ pages/app/[category]/[id].js
@@ -23,12 +23,12 @@ export default function Item(props) {
     setReview(event.target.value);
 
   }
-  
+
   useEffect(() => {
     if (!getLoggedIn()) router.push("/");
     else setUserData(JSON.parse(getCookie('userData')));
   }, []);
-  
+
 
   useEffect(() => {
     setReviewData(null);
@@ -39,27 +39,27 @@ export default function Item(props) {
 
     fetch(`/api/search/reviews/${activity_id}`, {
       method: "GET",
-      headers: {"Content-Type": "application/json"},
+      headers: { "Content-Type": "application/json" },
     })
-    .then((response) => response.json())
-    .then((reviewData) => {
-      setReviewData(reviewData);
-      console.log(reviewData);
-      
-    });
-    
+      .then((response) => response.json())
+      .then((reviewData) => {
+        setReviewData(reviewData);
+        // console.log(reviewData);
+
+      });
+
   }, [userData]);
 
   async function handleReviewClick(event, id) {
     event.preventDefault();
-    
+
     const pathname = window.location.pathname;
     const activity_id = pathname.split("/")[3]
 
     await fetch("/api/search/review/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ userID: userData.email, review: review, activity_id: activity_id}),
+      body: JSON.stringify({ userID: userData.email, review: review, activity_id: activity_id }),
     })
       .then((response) => response.json())
       .then((result) => {
@@ -75,23 +75,22 @@ export default function Item(props) {
 
     const pathname = window.location.pathname;
     const activity_id = pathname.split("/")[3]
-
+    console.log(activity_id);
     const response = await fetch("/api/search/review/", {
       method: "DELETE",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({review_id: id, activity_id: activity_id})
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ review_id: id, activity_id: activity_id })
     })
- 
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result)
-      if (result["results"]) {
-        alert("Review has been deleted!");
-        router.reload(window.location.pathname);
-      } else alert("Something went wrong!!!!!!!");
-    });
-  }
 
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result)
+        if (result["results"]) {
+          alert("Review has been deleted!");
+          router.reload(window.location.pathname);
+        } else alert("Something went wrong!!!!!!!");
+      });
+  }
 
   const view = props.category;
   const data = JSON.parse(props.data);
@@ -111,16 +110,29 @@ export default function Item(props) {
     if (reviewData != null) {
       if (reviewData.reviews != undefined) {
         return (
-          reviewData.reviews.map((data, i) => <div>
-          <button type="button"
-          onClick={async () => {
-             await handleReviewDelClick(data.review_id)
-           }}
-           className="float-right " 
-         > x </button>
-         <p>{data["email"]}</p>
-         <p>{data["review_desc"]}</p><br></br> 
-       </div>)
+          reviewData.reviews.map((data, i) =>
+          <>
+            {i == i && <div className="font-bold text-xl">Reviews</div>}
+            <div className="flex items-center justify-between shadow-sm mb-2">
+              <div className="flex flex-col">
+                <div>{data["email"]}</div>
+                <div>{data["review_desc"]}</div>
+              </div>
+              <div className="flex items-center gap-2">
+                {/* <div className="" onClick={() => handleReviewEditClick(data.review_id)}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                </svg>
+                </div> */}
+                <div className="" onClick={() => handleReviewDelClick(data.review_id)}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                  </svg>
+                </div>
+              </div>
+            </div>
+            </>
+          )
         )
       }
     }
@@ -216,25 +228,27 @@ export default function Item(props) {
             </div>
           </div> */}
         <div>
-          <div className=" container flex flex-col justify-start min-h-full gap-5 px-10 py-5">
+          <div className=" container flex flex-col justify-start min-h-full gap-5 px-5 py-5">
             {/* {reviewData != null
               ? {reviewData.reviews != undefined ? 
               : [...Array(5)].map((data, i)) }
                */}
-              {renderReviews()}
+            {renderReviews()}
+            <div className="flex flex-col items-start gap-4 justify-start">
+              <input
+                id="addReview"
+                type="text"
+                placeholder="Add Review"
+                className=" container flex flex-col items-center justify-start pl-2 py-3 rounded-sm ring-2 ring-sgg-input-gray focus:outline-sgg-blue"
+                rows="3"
+                onChange={reviewChangeHandler}
+              />
+              <button type="submit" onClick={handleReviewClick} className="w-30 p-2 px-5 text-white transition-colors rounded-sm bg-sgg-blue hover:bg-sgg-blue/20">
+                Add Review
+              </button>
+            </div>
           </div>
         </div>
-        <input
-          id="addReview"
-          type="text"
-          placeholder="Add Review"
-          className=" container flex flex-col items-center justify-start px-10 py-5 rounded-sm ring-2 ring-sgg-input-gray focus:outline-sgg-blue"
-          rows="3"
-          onChange={reviewChangeHandler}
-        />
-        <button type="submit" onClick={handleReviewClick} className="pl w-30 px-10 py-5 text-white transition-colors rounded-sm bg-sgg-blue hover:bg-sgg-blue/20">
-          Add Review
-        </button>
       </section>
     </Layout>
   );
